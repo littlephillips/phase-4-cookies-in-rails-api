24749d96be03851ef14e9dffb8f08865c17085ff
littlephillips <phillipkinuthia72@gmail.com>
2022-11-30 14:10:37 +0300

    accessing cookies in a rails api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   
   def index
     session[:session_hello] ||= "World"
-    cookies[:cookies_hello] ||= "World"
+    cookies[:cookies_hello] ||= "Phill"
     render json: { session: session, cookies: cookies.to_hash }
   end
 
